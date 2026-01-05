7a0f3b886c05f505df7b1f8df275e07ebeaafa7d
iurww <wwrui1023@outlook.com>
2026-01-05 11:46:27 +0800

    add ddp

--- src/model.py
+++ src/model.py
@@ -10,7 +10,7 @@ class SigLIPClassifier(nn.Module):
         
         logging.info(f"Loading SigLIP model: {model_name}")
         self.siglip = AutoModel.from_pretrained(model_name)
-        self.siglip.requires_grad_(False) 
+        # self.siglip.requires_grad_(False) 
         # print(self.siglip)
         
         self.embed_dim = self.siglip.config.vision_config.hidden_size
